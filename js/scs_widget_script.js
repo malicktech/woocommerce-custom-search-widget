6acd7436674485f7113ce1d361a3449013964a85
Louis <louisdiatta0@gmail.com>
2018-12-13 10:37:57 +0000

    Removed loader animation

--- js/scs_widget_script.js
+++ js/scs_widget_script.js
@@ -8,9 +8,7 @@ jQuery(document).ready(function($){
     if(category == -1) {
       return;
     }
-    // $(".iboo-association-saved-label").show();
-    // $(".iboo-association-saved-label").text('En cours d\'enregistrement ...');
-    $(".loader").show();
+
     $.ajax({
         type: 'POST',
         url: ajax_object.ajax_url,
@@ -20,13 +18,9 @@ jQuery(document).ready(function($){
         },
         success: function (response) {
           $("#saona-custom-search-results").html(response.data);
-          $(".loader").hide();
-          // $(".iboo-association-saved-label").text('Enregistré');
-          // $(".iboo-association-saved-label").fadeOut(2000);
         },
         error: function( error) {
           console.log(error);
-          $(".loader").hide();
         }
     });
 
--- saona-custom-search.php
+++ saona-custom-search.php
@@ -72,9 +72,7 @@ class scs_widget extends WP_Widget {
 		echo '<select id="saona-custom-search-results" class="form-control">';
             get_products_by_category();
 		echo '</select>';
-		echo '<div style="display: none" class="loader"> </div>';
-		
-		echo '<div/>';
+		echo '</div>';
 
 	}
 
